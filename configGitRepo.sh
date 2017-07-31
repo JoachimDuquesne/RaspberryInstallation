7671f80fd8fd5a8af9ac987e20c6050a9981f7f2
EscapeRush <30594239+EscapeRush@users.noreply.github.com>
2017-07-31 18:50:23 +0200

    Create configGitRepo.sh

---
+++ configGitRepo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Verify that the user is EscapeRush for file tree consistancy
+if [ $USER != 'EscapeRush' ] ; then
+    echo Error: User is not \'EscapeRush\'
+    exit
+fi
+
+git config --global user.name EscapeRush
+git config --global user.email vincent@escaperush.com
+
+read -p "Enter repo to be configured :" repo
+
+git clone "https://github.com/EscapeRush/$repo.git"
