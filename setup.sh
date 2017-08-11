74208dc23d638880cc6d38a3eceac638cadd1f10
JoachimDuquesne <13562415+JoachimDuquesne@users.noreply.github.com>
2017-08-11 15:32:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # To install on a RaspberryPi for Escape Rush fresh 
 # Burn Raspbian image
+# Install Git : sudo apt-get install git
 # run : git clone  https://github.com/EscapeRush/RaspberryInstallation.git
 # then cd RaspberryInstallation
 #      chmod +x setup.sh
@@ -20,6 +21,7 @@
 # edit with nano /boot/cmdline.txt : add logo.nologo at the end
 #
 # execute the cmd below by executing : sudo ./setup.sh
+# edit with nano /boot/config.txt : uncomment disableoverscan=1 and set overscan values to 0 (uncommented)
 
 useradd -m -G adm,dialout,sudo,audio,video,plugdev,users,input,netdev,gpio,i2c,spi EscapeRush
 passwd EscapeRush
