1b8ed106e76d2f6bbff5f28d6d6014cba3d95340
JoachimDuquesne <13562415+JoachimDuquesne@users.noreply.github.com>
2017-09-10 12:48:05 +0200

    Adapted for metabaron user

--- configGitRepo.sh
+++ configGitRepo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Verify that the user is EscapeRush for file tree consistancy
-if [ $USER != 'EscapeRush' ] ; then
-    echo Error: User is not \'EscapeRush\'
+if [ $USER != 'metabaron' ] ; then
+    echo Error: User is not \'metabaron\'
     exit
 fi
 
-git config --global user.name EscapeRush
-git config --global user.email vincent@escaperush.com
-git config --global push.default simple
+git config --global user.name JoachimDuquesne
+git config --global user.email joachim.duquesne@gmail.com
+git config --global push.default matching
 
 read -p "Enter repo to be configured :" repo
 
-git clone "https://github.com/EscapeRush/$repo.git"
+git clone "https://github.com/JoachimDuquesne/$repo.git"
