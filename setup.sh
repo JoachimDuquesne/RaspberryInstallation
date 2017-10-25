c956d43b2213d0bcce59a4f0599c801c640d04a1
JoachimDuquesne <13562415+JoachimDuquesne@users.noreply.github.com>
2017-10-25 17:20:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # To install on a RaspberryPi for Escape Rush fresh
-# Burn Raspbian image
+# Burn Raspbian JESSIE image
+# http://downloads.raspberrypi.org/raspbian/images/raspbian-2017-07-05/
 #
 # in raspi-config : boot into CLI, no login
 #                   change hostname
@@ -25,21 +26,5 @@
 #
 # execute the cmd below by executing : sudo ./setup.sh
 
-if [ $# -ne 1 ]; then
-    echo "Usage : sudo ./setup.sh USERNAME"
-    exit 1
-fi
-
 aptitude update && aptitude full-upgrade
 aptitude install -y -r omxplayer arduino screen wiringPi libpam-systemd python-serial telnet mosquitto mosquitto-clients libmosquitto-dev qt5-default libpcre++-dev libboost-dev libdbus-1-dev 
-
-useradd -m -G adm,dialout,systemd-journal,sudo,audio,video,plugdev,users,input,netdev,gpio,i2c,spi,www-data $1
-echo "$1:raspberry" | chpasswd     # Setup default password
-passwd -e $1    # Force new password at first login
-
-cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
-cp interfaces /etc/network/interfaces
-
-#userdel -f -r pi
-
-reboot
